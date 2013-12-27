10370a8fcc01e732acd6d9c3ffc66df3e79b2eb1
Badhashsolution <badhashsolution@gmail.com>
2013-12-27 06:16:07 +0000

    found a type in urls.py

--- microblog/urls.py
+++ microblog/urls.py
@@ -17,7 +17,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
 
-    url(r'$', TemplateView.as_view(template_name="index.html")),
+    url(r"^$", TemplateView.as_view(template_name="index.html")),
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
