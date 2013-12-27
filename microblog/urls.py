c9334f86950871e90df9e6c73071355049ba1d1d
Badhashsolution <badhashsolution@gmail.com>
2013-12-27 06:21:48 +0000

    removed some comments from urls.py

--- microblog/urls.py
+++ microblog/urls.py
@@ -9,15 +9,6 @@ admin.autodiscover()
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'microblog.views.home', name='home'),
-    # url(r'^microblog/', include('microblog.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-
     url(r"^$", TemplateView.as_view(template_name="index.html")),
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
